f12e7330a75d21e7073572aa477fdfd8ee58ae45
Elizabeth Tackett <emctackett@gmail.com>
2018-09-07 09:43:53 -0500

    collect nytimes us headlines

--- buzzwords.rb
+++ buzzwords.rb
@@ -1,12 +1,29 @@
+require 'mechanize'
+
 require_relative 'ny_times.rb'
 
 class Buzzwords
   attr_reader :nytimes
+  attr_accessor :aggregate_headlines
+
+  MECH = Mechanize.new
 
   def initialize
     @nytimes = NYTimes.new
+    @aggregate_headlines = []
+    generate_buzz
+  end
+
+  private
+
+  def retrieve_nytimes_headlines
+    self.aggregate_headlines += nytimes.headlines
+  end
+
+  def generate_buzz
+    retrieve_nytimes_headlines
   end
 end
 
 buzzwords = Buzzwords.new
-p buzzwords.nytimes.test
+p buzzwords.aggregate_headlines
--- ny_times.rb
+++ ny_times.rb
@@ -1,7 +1,16 @@
 class NYTimes
-  attr_reader :test
+  attr_reader :test, :webpage, :headlines
+
+  US_HEADLINE_URL = "https://www.nytimes.com/section/us"
 
   def initialize
     @test = 'test'
+    @webpage = Buzzwords::MECH.get(US_HEADLINE_URL)
+  end
+
+  def headlines
+    webpage.search('.initial-set .headline').map do |headline|
+      headline.text.strip
+    end
   end
 end
