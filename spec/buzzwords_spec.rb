7590aa810bb26b491192406074147b011a1bf5e4
Elizabeth Tackett <emctackett@gmail.com>
2018-10-08 11:42:10 -0500

    add test example for parsing headlines

--- spec/buzzwords_spec.rb
+++ spec/buzzwords_spec.rb
@@ -23,16 +23,33 @@ describe 'buzzwords' do
         expect(Buzzwords.aggregate_headlines).not_to be_empty
       end
     end
+
     context '.retrieve_cnn_headlines' do
       it 'adds CNN headlines to aggregate' do
         Buzzwords.retrieve_cnn_headlines
         expect(Buzzwords.aggregate_headlines).not_to be_empty
       end
     end
+
     context '.retrieve_reuters_headlines' do
       it 'adds Reuters headlines to aggregate' do
         Buzzwords.retrieve_reuters_headlines
         expect(Buzzwords.aggregate_headlines).not_to be_empty
+      end
+    end
+  end
+
+  context '.parse_headlines' do
+    before(:all) { @headlines = Buzzwords.parse_headlines(NYTimes.new.headlines) }
+
+    it 'should return an array' do
+      expect(@headlines).to be_an_instance_of(Array)
+    end
+
+    it 'should only include properly formatted words' do
+      @headlines.each do |headline|
+        expect(headline).not_to include('/(\A[\W]||[\W]\z)/')
+        expect(headline).not_to include('/\'s/')
       end
     end
   end
